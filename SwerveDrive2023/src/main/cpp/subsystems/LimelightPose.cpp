c3bd7d1d5a2897301d00c0c58f6ee9c3baaa5977
= <=>
2023-11-28 16:59:04 -0600

    adding limelight subsystem to work with apriltag

---
+++ SwerveDrive2023/src/main/cpp/subsystems/LimelightPose.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "subsystems/LimelightPose.h"
+
+LimelightPose::LimelightPose() = default;
+
+// This method will be called once per scheduler run
+void LimelightPose::Periodic() {
+
+    // frc::SmartDashboard::GetValue("botpose");
+}
